ed3facff3cb69eb75267af4ac571a55783f6d442
lilKriT <lilKriTwebDev@gmail.com>
2026-02-03 03:58:09 +0100

    Working on data.

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${barlowSemiCondensed.variable} antialiased`}>
-        {children}
+        <main>{children}</main>
       </body>
     </html>
   );
