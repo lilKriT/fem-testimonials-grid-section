0cabe4dd8031650dae15d6a9659c323a9c6db5cf
lilKriT <lilKriTwebDev@gmail.com>
2026-02-07 23:19:33 +0100

    Lots of fixes.

--- app/_components/TestimonialCard.tsx
+++ app/_components/TestimonialCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Testimonial } from "../page";
 
 type Props = {
@@ -9,10 +10,24 @@ const TestimonialCard = ({ testimonial }: Props) => {
 
   return (
     <div
-      className={`${t.wide ? "col-span-6" : "col-span-3"} ${t.tall ? "row-span-2" : "row-span-1"} p-8 rounded-3xl shadow-[30px_30px_50px_0px_rgba(0,0,0,30%)] ${t.style}`}
+      className={`${t.wide ? "col-span-6" : "col-span-3"} ${t.tall ? "row-span-2" : "row-span-1"} flex flex-col gap-4 p-8 rounded-3xl shadow-[30px_30px_50px_0px_rgba(0,0,0,30%)] ${t.style}`}
     >
-      <h2>{t.name}</h2>
-      <p>{t.highlight}</p>
+      <div className="flex items-center gap-4">
+        {t.picture && (
+          <Image
+            src={`/${t.picture}`}
+            alt={`Picture of ${t.name}`}
+            width={56}
+            height={56}
+            className="rounded-full w-10 aspect-square"
+          />
+        )}
+        <div className="flex flex-col items-start">
+          <h2 className="font-semibold">{t.name}</h2>
+          <p>{t.verified}</p>
+        </div>
+      </div>
+      <p className="text-xl font-semibold leading-7">{t.highlight}</p>
       <p>{t.quote}</p>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import TestimonialCard from "./_components/TestimonialCard";
 export type Testimonial = {
   name: string;
   verified: string;
-  picture?: string;
+  picture: string;
   highlight: string;
   quote: string;
   style?: string;
@@ -21,7 +21,7 @@ const testimonials: Testimonial[] = [
     quote:
       "“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”",
     wide: true,
-    style: "bg-purple-500 text-gray-100",
+    style: "purple",
   },
   {
     name: "Jonathan Walters",
@@ -30,7 +30,7 @@ const testimonials: Testimonial[] = [
     highlight: "The team was very supportive and kept me motivated",
     quote:
       "“ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company. This was one of the best investments I’ve made in myself. ”",
-    style: "bg-gray-400",
+    style: "gray",
   },
   {
     name: "Kira Whittle",
@@ -40,7 +40,7 @@ const testimonials: Testimonial[] = [
     quote:
       " “ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial could ever have. In fact, I’ve often referred to it during interviews as an example of my development experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 100% recommend! ”",
     tall: true,
-    style: "bg-white",
+    style: "white",
   },
   {
     name: "Jeanette Harmon",
@@ -49,7 +49,7 @@ const testimonials: Testimonial[] = [
     highlight: "An overall wonderful and rewarding experience",
     quote:
       "“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love. ”",
-    style: "bg-white",
+    style: "white",
   },
   {
     name: "Patrick Abrams",
@@ -60,15 +60,15 @@ const testimonials: Testimonial[] = [
     quote:
       "“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people. ”",
     wide: true,
-    style: "bg-darkblue",
+    style: "dark",
   },
 ];
 
 export default function Home() {
   return (
     <section className="min-h-dvh flex justify-center items-center">
-      <div className="min-w-sm max-w-5xl">
-        <div className="grid grid-cols-12 grid-rows-2 gap-8">
+      <div className="min-w-sm max-w-5xl my-32">
+        <div className="grid grid-cols-12 grid-rows-2 gap-x-8 gap-y-6">
           {testimonials.map((t, idx) => (
             <TestimonialCard key={idx} testimonial={t} />
           ))}
